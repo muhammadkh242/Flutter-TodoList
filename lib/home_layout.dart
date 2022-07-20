959afe6d720139784ec617b5c436da936ed22f81
mohamedkh242 <mohamedkholif710@gmail.com>
2022-07-21 01:20:31 +0200

    finish add new task bottom sheet

--- lib/home_layout.dart
+++ lib/home_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:todo/module/archived/archived_screens.dart';
 import 'package:todo/module/done/done_screen.dart';
@@ -12,7 +13,6 @@ class HomeLayout extends StatefulWidget {
 }
 
 class _HomeLayoutState extends State<HomeLayout> {
-
   int currentIndex = 0;
   List<Widget> pages = [
     const TasksScreen(),
@@ -27,8 +27,11 @@ class _HomeLayoutState extends State<HomeLayout> {
   ];
 
   late Database db;
-  var scaffoldKey  = GlobalKey<ScaffoldState>();
+  var scaffoldKey = GlobalKey<ScaffoldState>();
   bool isBottomSheet = false;
+  Icon fabIcon = const Icon(Icons.add);
+  var isLow = false;
+  var formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -39,43 +42,54 @@ class _HomeLayoutState extends State<HomeLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       key: scaffoldKey,
       appBar: AppBar(
         title: Text(titles[currentIndex]),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          scaffoldKey.currentState?.showBottomSheet((context){
+          if (isBottomSheet) {
+            if(formKey.currentState!.validate()){
+              Navigator.pop(context);
+              isBottomSheet = false;
+              setState(() {
+                fabIcon = const Icon(Icons.add);
+              });
+            }
 
-            return Container(
-              height: 100.0,
-              color: Colors.red,
+          } else {
+            scaffoldKey.currentState?.showBottomSheet(
+              (context) => Form(
+                key: formKey,
+                child: const MyBottomSheet()
+              ),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(30.0),
+                )
+              ),
+              elevation: 180.0,
             );
-
-          });
+            isBottomSheet = true;
+            setState(() {
+              fabIcon = const Icon(Icons.check);
+            });
+          }
         },
-        child: isBottomSheet? const Icon(Icons.check) : const Icon(Icons.add),
+        child: fabIcon,
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: currentIndex,
-        items:
-        const [
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Tasks"),
           BottomNavigationBarItem(
-            icon: Icon(Icons.menu),
-            label: "Tasks"
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.check_circle_sharp),
-            label: "Done"
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.archive),
-            label: "Archived"
-          ),
+              icon: Icon(Icons.check_circle_sharp), label: "Done"),
+          BottomNavigationBarItem(icon: Icon(Icons.archive), label: "Archived"),
         ],
-        onTap: (index){
-          setState((){
+        onTap: (index) {
+          setState(() {
             currentIndex = index;
           });
         },
@@ -84,37 +98,219 @@ class _HomeLayoutState extends State<HomeLayout> {
     );
   }
 
-  void createDB() async{
-    db = await openDatabase(
-      'tasks.db',
-      version: 1,
 
-      onCreate: (db, version){
-        print("db created");
-        db.execute('CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)').then((value){
-          print("created table");
-        }).catchError((error){
-          print(error.toString());
-        });
-      },
-
-      onOpen: (db){
-        print("opened db");
-      }
-    );
-
+  // ___________________________LOCAL DB_____________________
+  void createDB() async {
+    db = await openDatabase('tasks.db', version: 1, onCreate: (db, version) {
+      print("db created");
+      db
+          .execute(
+              'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)')
+          .then((value) {
+        print("created table");
+      }).catchError((error) {
+        print(error.toString());
+      });
+    }, onOpen: (db) {
+      print("opened db");
+    });
   }
 
-  void insertIntoDB(){
-    db.transaction((txn){
-      txn.rawInsert('INSERT INTO tasks (title, date, time, status) VALUES ("FIRST", "TIME ONE", "DATE ONE", "STATUS ONE")')
-      .then((value){
-        print("$value inserted") ;
-      })
-      .catchError((error){
+  void insertIntoDB() {
+    db.transaction((txn) {
+      txn
+          .rawInsert(
+              'INSERT INTO tasks (title, date, time, status) VALUES ("FIRST", "TIME ONE", "DATE ONE", "STATUS ONE")')
+          .then((value) {
+        print("$value inserted");
+      }).catchError((error) {
         print(error.toString());
       });
       return Future(() => null);
     });
+  }
+}
+
+
+//_______________BOTTOM SHEET_________________________
+class MyBottomSheet extends StatefulWidget {
+  const MyBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  State<MyBottomSheet> createState() => _MyBottomSheetState();
+}
+
+class _MyBottomSheetState extends State<MyBottomSheet> {
+  bool isLow = false;
+  bool isMedium = false;
+  bool isHigh = false;
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController timeController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0)
+        )
+      ),
+      padding: const EdgeInsets.all(15.0),
+      height: 380.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            controller: titleController,
+            decoration: const InputDecoration(
+                label: Text("Title"),
+                prefixIcon: Icon(Icons.title),
+                border: OutlineInputBorder()
+            ),
+            validator: (value){
+              if(value!.isEmpty){
+                return "Title must not be Empty";
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          TextFormField(
+            controller: timeController,
+            keyboardType: TextInputType.datetime,
+            onTap: (){
+              showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now()
+              ).then((value){
+                timeController.text = value!.format(context).toString();
+              });
+            },
+            decoration: const InputDecoration(
+                label: Text("Time"),
+                prefixIcon: Icon(Icons.watch_later_outlined),
+                border: OutlineInputBorder()
+            ),
+            validator: (value){
+              if(value!.isEmpty){
+                return "Time must not be Empty";
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          TextFormField(
+            controller: dateController,
+            keyboardType: TextInputType.datetime,
+            onTap: (){
+              showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime.now().add(const Duration(days: 30))
+              ).then((value){
+                dateController.text = DateFormat.yMMMEd().format(value!);
+              });
+            },
+            decoration: const InputDecoration(
+                label: Text("Date"),
+                prefixIcon: Icon(Icons.date_range_outlined),
+                border: OutlineInputBorder()
+            ),
+            validator: (value){
+              if(value!.isEmpty){
+                return "Date must not be Empty";
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          const Text(
+            "Priority",
+            style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue),
+          ),
+          const SizedBox(
+            height: 5.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ChoiceChip(
+                padding: const EdgeInsets.all(10.0),
+                label: const Text(
+                  "Low",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+                ),
+                selected: isLow,
+                onSelected: (isSelected) {
+                  setState(() {
+                    isLow = isSelected;
+                    isMedium = false;
+                    isHigh = false;
+                  });
+                },
+                selectedColor: Colors.blue,
+              ),
+              const SizedBox(
+                width: 10.0,
+              ),
+              ChoiceChip(
+                padding: const EdgeInsets.all(10.0),
+                label: const Text(
+                  "Medium",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+                ),
+                selected: isMedium,
+                onSelected: (isSelected) {
+                  setState(() {
+                    print(isSelected);
+                    isMedium = isSelected;
+                    isLow = false;
+                    isHigh = false;
+                  });
+                },
+                selectedColor: Colors.blue,
+              ),
+              const SizedBox(
+                width: 10.0,
+              ),
+              ChoiceChip(
+                padding: const EdgeInsets.all(10.0),
+                label: const Text(
+                  "High",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+                ),
+                selected: isHigh,
+                onSelected: (isSelected) {
+                  setState(() {
+                    print(isSelected);
+                    isHigh = isSelected;
+                    isMedium = false;
+                    isLow = false;
+                  });
+                },
+                selectedColor: Colors.blue,
+              ),
+              const SizedBox(
+                width: 10.0,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
